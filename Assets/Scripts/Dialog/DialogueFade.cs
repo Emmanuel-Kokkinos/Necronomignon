8f928a20bed5a4824404f3d52e20c6e87dc03208
CathGitHub <catherine.biella@gmail.com>
2021-03-15 10:42:35 -0400

    fade

---
+++ Assets/Scripts/Dialog/DialogueFade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueFade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fade()
+    {
+        Color colour = Color.black;
+
+        Initiate.Fade("DialogScene", colour, 1.5f);
+    }
+}
