65acad54dc923a508dddfd06e9b9724557539de4
MiguelACG <63476768+MiguelACG@users.noreply.github.com>
2021-03-23 18:11:18 -0400

    minor kitsune fix

--- Assets/Scripts/Battle/Beasts/Kitsune_Script.cs
+++ Assets/Scripts/Battle/Beasts/Kitsune_Script.cs
@@ -7,21 +7,35 @@ public class Kitsune_Script : MonoBehaviour, Parent_Beast
     BattleManager battleManager;
     Attack attack;
     [SerializeField] GameObject backPrefab;
+    float shootFrame;
 
     void Start()
     {
+        
         GameObject g = GameObject.Find("GameManager");
-
+        GameObject au = GameObject.Find("Music");
         if (g != null)
         {
             battleManager = g.GetComponent<BattleManager>();
             attack = g.GetComponent<Attack>();
         }
+
     }
 
     public void back_special()
     {
-        ProjectileAnimation();
+        RuntimeAnimatorController animCont = this.GetComponent<Animator>().runtimeAnimatorController;
+
+        AnimationClip kitsuneBack = animCont.animationClips[2];
+
+        //Calculate the time at which the frame of the animation is called
+        float frame = 34 * (1 / 30);
+
+        print("frame " + frame);
+        shootFrame = kitsuneBack.length - frame;
+        //shootFrame += Time.time;
+        print("shoot :" + shootFrame);
+        StartCoroutine(ProjectileAnimation());
     }
 
     public void front_special() 
@@ -38,20 +52,28 @@ public class Kitsune_Script : MonoBehaviour, Parent_Beast
         }
     }
 
-    void ProjectileAnimation()
+    IEnumerator ProjectileAnimation()
     {
+        
         GameObject player = battleManager.getSlot(battleManager.currentTurn);
         GameObject target = battleManager.getSlot(battleManager.targets[0]);
 
         GameObject movePrefab = Instantiate(backPrefab);
+        
         movePrefab.transform.SetParent(player.transform);
         movePrefab.transform.localPosition = new Vector3(0, 0);
         movePrefab.transform.localRotation = Quaternion.identity;
         movePrefab.transform.localScale = new Vector3(30, 30);
 
+
+
+        yield return new WaitForSecondsRealtime(5);
+
         Vector3 shootDir = ((target.transform.localPosition) - (player.transform.localPosition)).normalized;
 
+  
         movePrefab.GetComponent<Projectile>().Setup(shootDir);
+
     }
 
     public void Play_SoundFX()
