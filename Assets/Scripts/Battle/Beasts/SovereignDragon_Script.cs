fba16023e6a3e5cdf15c83cfa32c67a832c6cf93
MiguelACG <63476768+MiguelACG@users.noreply.github.com>
2021-03-29 17:57:32 -0400

    Some sounds added

--- Assets/Scripts/Battle/Beasts/SovereignDragon_Script.cs
+++ Assets/Scripts/Battle/Beasts/SovereignDragon_Script.cs
@@ -8,7 +8,7 @@ public class SovereignDragon_Script : MonoBehaviour, Parent_Beast
     BattleManager battleManager;
     Attack attack;
     [SerializeField] GameObject frontPrefab;
-    [SerializeField] AudioClip frontAttackSound, backAttackSound, startSound, deathSound;
+    [SerializeField] AudioClip frontAttackSound, backAttackSound, damageSound, deathSound;
     AudioSource audioSrc;
 
     void Start()
@@ -68,14 +68,12 @@ public class SovereignDragon_Script : MonoBehaviour, Parent_Beast
 
     public void Play_SoundFX(string sound)
     {
-        /*
         switch (sound)
         {
             case "front": audioSrc.PlayOneShot(frontAttackSound); break;
             case "back": audioSrc.PlayOneShot(backAttackSound); break;
-            //case "start": audioSrc.PlayOneShot(startSound); break;
+            case "damage": audioSrc.PlayOneShot(damageSound); break;
             case "death": audioSrc.PlayOneShot(deathSound); break;
         }
-        */
     }
 }
