655959fa808adda235b787e95b79835908d6aaf5
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2021-04-07 12:43:55 -0400

    test

---
+++ Assets/EarthDragon.cs
@@ -0,0 +1,30 @@
+﻿using DragonBones;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthDragon : MonoBehaviour
+{
+    UnityArmatureComponent armatureComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        armatureComponent = GetComponent<UnityArmatureComponent>();
+        armatureComponent.animation.FadeIn("Idle", 2f, -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PlayAttackA();
+        }
+    }
+
+    void PlayAttackA()
+    {
+        armatureComponent.animation.FadeIn("Attack A", 0, 1);
+    }
+}
