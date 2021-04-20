98b704925f4f7040951e5c1a0f14929ed9cbb0fb
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2021-04-20 16:30:31 -0400

    Added Shoggoth

---
+++ Assets/Scripts/Battle/Beasts/Shoggoth_Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoggoth_Script : Parent_Script, Parent_Beast
+{
+    [SerializeField] AudioClip frontAttackSound, backAttackSound, damageSound, deathSound;
+    AudioSource audioSrc;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+
+        base.start();
+    }
+
+    public void back_special()
+    {
+
+    }
+
+    public void front_special()
+    {
+
+    }
+
+    public void Play_SoundFX(string sound)
+    {
+
+        switch (sound)
+        {
+            case "front": audioSrc.PlayOneShot(frontAttackSound); break;
+            case "back": audioSrc.PlayOneShot(backAttackSound); break;
+            case "damage": audioSrc.PlayOneShot(damageSound); break;
+            case "death": audioSrc.PlayOneShot(deathSound); break;
+        }
+
+    }
+}
