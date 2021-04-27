adab0b4445744487e10cf0d7408bb0068ca9b036
Val <60553975+D-Valentina@users.noreply.github.com>
2021-04-27 14:05:53 -0400

    Added beam and move animations back

--- Assets/Scripts/Battle/Beasts/Mandoro_Script.cs
+++ Assets/Scripts/Battle/Beasts/Mandoro_Script.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
  
 public class Mandoro_Script : Parent_Script, Parent_Beast
 {
     [SerializeField] AudioClip frontAttackSound, backAttackSound, damageSound, deathSound;
-
+    private GameObject emptyObj;
     public void back_special()
     {
-        int ran = Random.Range(1, 5);
+        int ran = UnityEngine.Random.Range(1, 5);
         print("The number of attacks is " + (ran + 1));
         foreach (Beast b in battleManager.targets)
         {
@@ -34,6 +36,8 @@ public class Mandoro_Script : Parent_Script, Parent_Beast
         {
             attack.InitiateAttack(battleManager.currentTurn, battleManager.targets, battleManager.inFront(), battleManager.enemySummoner);
         }
+
+        PlayBackMove();
     }
 
     public void front_special() 
@@ -53,6 +57,59 @@ public class Mandoro_Script : Parent_Script, Parent_Beast
 
     //To modify -> add parameter to select character sound.
 
+    public async void PlayBackMove()
+    {
+        await Task.Delay(500);
+
+        GameObject player = this.gameObject;
+        GameObject target = battleManager.getSlot(battleManager.targets[0]);
+        emptyObj = new GameObject("Empty");
+        GameObject arm = player.transform.Find("Left Arm").gameObject;
+
+        float playerX = player.transform.position.x;
+        float playerY = player.transform.position.y;
+        float targetX = target.transform.position.x;
+        float targetY = target.transform.position.y;
+        targetY += targetY > playerY ? -10 : 10;
+
+        float deltaX = playerX - targetX;
+        float deltaY = playerY - targetY;
+
+        float delta = deltaY / deltaX;
+        double tan = Mathf.Atan(delta);
+
+        double angle = tan * (180 / Math.PI);
+
+        //Debug.Log("playerX + playerY: " + playerX + " " + playerY);
+        //Debug.Log("targetX + targetY : " + targetX + " " + targetY);
+        //Debug.Log("deltaX + deltaY : " + deltaX + " " + deltaY);
+        //Debug.Log("delta : " + delta);
+        //Debug.Log("Angle : " + angle);
+
+        Component[] allComp = emptyObj.GetComponents<Component>();
+        if (allComp.Length == 1)
+        {
+            Debug.Log("fuck");
+            emptyObj = new GameObject("Empty");
+            emptyObj.transform.SetParent(arm.transform);
+            emptyObj.transform.localPosition = new Vector2(-1.7f, 2.5f);
+            emptyObj.transform.localScale = new Vector2(0.2f, 0.2f);
+        }
+
+        GameObject movePrefab = (GameObject)Instantiate(Resources.Load("Prefabs/Moves/Move"));
+        movePrefab.transform.SetParent(emptyObj.transform);
+
+
+        movePrefab.transform.localPosition = new Vector2(133, 0);
+        movePrefab.transform.localScale = new Vector2(playerX > targetX ? 1.5f : -1.5f, 0.1f);
+        emptyObj.transform.rotation = Quaternion.Euler(0, 0, (float)angle);
+        movePrefab.transform.SetAsFirstSibling();
+
+        await Task.Delay(70);
+
+        //movePrefab.GetComponent<Animator>().runtimeAnimatorController = Resources.Load("Animations/Mandoro/Mandoro_Move_Controller") as RuntimeAnimatorController;
+        movePrefab.GetComponent<Animator>().SetTrigger("Back");
+    }
 
     public void Play_SoundFX(string sound)
     {
